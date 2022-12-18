38c624a8327b8210a41ba635b8ce7b51f892c748
adityashas <aditya_shas@yahoo.com>
2022-12-18 22:58:50 +0530

    commit

---
+++ login.py
@@ -0,0 +1,124 @@
+class User(db.Document):
+    user_id     =   db.IntField( unique=True )
+    first_name  =   db.StringField( max_length=50 )
+    last_name   =   db.StringField( max_length=50 )
+    email       =   db.StringField( max_length=60, unique=True )
+    password    =   db.StringField( )
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
+
+class LoginForm(FlaskForm):
+    email   = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6,max=15)])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    email   = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(),Length(min=6,max=15)])
+    password_confirm = PasswordField("Confirm Password", validators=[DataRequired(),Length(min=6,max=15), EqualTo('password')])
+    first_name = StringField("First Name", validators=[DataRequired(),Length(min=2,max=55)])
+    last_name = StringField("Last Name", validators=[DataRequired(),Length(min=2,max=55)])
+    submit = SubmitField("Register Now")
+
+    def validate_email(self,email):
+        user = User.objects(email=email.data).first()
+        if user:
+            raise ValidationError("Email is already in use. Pick another one.")
+
+
+@api.route('/api','/api/')
+class GetAndPost(Resource):
+    #GET ALL
+    def get(self):
+        return jsonify(User.objects.all())
+
+    #POST
+    def post(self):
+        data = api.payload
+        user = User(user_id=data['user_id'], email=data['email'], first_name=data['first_name'], last_name=data['last_name'])
+        user.set_password(data['password'])
+        user.save()
+        return jsonify(User.objects(user_id=data['user_id']))
+
+@api.route('/api/<idx>')
+class GetUpdateDelete(Resource):
+
+    #GET ONE
+    def get(self,idx):
+        return jsonify(User.objects(user_id=idx))
+
+    #PUT
+    def put(self,idx):
+        data = api.payload
+        User.objects(user_id=idx).update(**data)
+        return jsonify(User.objects(user_id=idx))
+
+    #DELETE
+    def delete(self,idx):
+        User.objects(user_id=idx).delete()
+        return jsonify("User is deleted!")
+        
+        @app.route("/login", methods=['GET','POST'])
+def login():
+    if session.get('username'):
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        email       = form.email.data
+        password    = form.password.data
+
+        user = User.objects(email=email).first()
+        if user and user.get_password(password):
+            #flash(f"{user.first_name}, you are successfully logged in!", "success")
+            session['user_id'] = user.user_id
+            session['username'] = user.first_name
+            return redirect("/user")
+        else:
+            flash("Sorry, something went wrong.","danger")
+    return render_template("login.html", title="Login", form=form, login=True )
+    
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("index.html", index=True )
+
+@app.route("/logout")
+def logout():
+    session['user_id']=False
+    session.pop('username',None)
+    flash("Successfully logged out!","success")
+    return redirect(url_for('index'))
+
+@app.route("/register", methods=['POST','GET'])
+def register():
+    if session.get('username'):
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_id     = User.objects.count()
+        user_id     += 1
+
+        email       = form.email.data
+        password    = form.password.data
+        first_name  = form.first_name.data
+        last_name   = form.last_name.data
+
+        user = User(user_id=user_id, email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        flash("You are successfully registered!","success")
+        return redirect(url_for('index'))
+    return render_template("register.html", title="Register", form=form, register=True)
+
+@app.route("/user")
+def user():
+    if not session.get('username'):
+        return redirect(url_for('login'))
+    return render_template("user.html",title="Admin Page", user=True)
