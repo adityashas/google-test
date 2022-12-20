2e11825217a5ee9eff110273b5a97c89e3475c24
adityashas <aditya_shas@yahoo.com>
2022-12-20 17:10:38 +0530

    commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask import render_template, request, json, jsonify, Response, redirect, flash, url_for, session
-import flask.scaffold
-flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+#import flask.scaffold
+#flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask_restplus import Resource
 from flask_restplus import Api
 from flask_wtf import FlaskForm
